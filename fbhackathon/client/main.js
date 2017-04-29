82c42feeacd751c58dbef0354dcea4436269291b
Mendel Liang <mendel.liang@live.com.au>
2017-04-29 15:09:25 +1000

    moved login/register/logout logic to loginRegister.js

--- fbhackathon/client/main.js
+++ fbhackathon/client/main.js
@@ -1,28 +1,8 @@
 import { Template } from 'meteor/templating';
 
 import './main.html';
-import './routes.js'
-
-
-
-Template.register.events({
-    'submit form': function(event){
-        event.preventDefault();
-        var username = $('[name=username]').val();
-        var password = $('[name=password]').val();
-        Accounts.createUser({
-            username: username,
-            password: password
-        }, function(error){
-    		if(error){
-        		alert(error.reason); // Output error if registration fails
-    		} 
-    		else{
-    			Router.go('/home');
-    		}
-		});
-    }
-});
+import './routes.js';
+import './loginRegister.js';
 
 Template.Home.helpers({
   currentUser: function() {
@@ -31,26 +11,5 @@ Template.Home.helpers({
 })
 
 
-Template.navigation.events({
-    'click .logout': function(event){
-        event.preventDefault();
-        Meteor.logout();
-    	Router.go('/login');
-    }
-});
 
-Template.login.events({
-    'submit form': function(event){
-        event.preventDefault();
-        var username = $('[name=username]').val();
-        var password = $('[name=password]').val();
-        Meteor.loginWithPassword(username, password, function(error){
-        	if(error){
-    			alert(error);
-    		}
-    		else{
-    			Router.go('/home');
-    		}
-        });
-    }
-});
+

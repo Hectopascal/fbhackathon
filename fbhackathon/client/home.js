4f757fae2f782dd71d12f2555d9d2488a8c9fd43
junrui93 <chenjunrui93@hotmail.com>
2017-04-30 07:14:52 +1000

    fix bugs

--- fbhackathon/client/home.js
+++ fbhackathon/client/home.js
@@ -1,5 +1,7 @@
 Template.Home.events({
     'click .join': function(event){
-        Router.go('/player?audioId=' + $(event.target).attr('audioId') + '&audioName=' + $(event.target).attr('audioName'));
+        var next = '/player?audioId=' + $(event.target).attr('audioId') + '&audioName=' + $(event.target).attr('audioName');
+        //Router.go(next);
+        location.href = next;
     }
 });
--- fbhackathon/client/timeline.js
+++ fbhackathon/client/timeline.js
@@ -1,23 +1,21 @@
 import { uploads, Audio } from '../api/collections.js';
-import aeditorInit from './aeditorInit.js';
-import aeditorEvent from './aeditorEvent';
 
-import './audiojs/audio.min.js';
-var playlist = undefined;
-
-Template.Home.onRendered(function (){
-    playlist = aeditorInit();
-    aeditorEvent(playlist);
-    console.log('lol');
-
-    audiojs.events.ready(function() {
-        var as = audiojs.createAll();
-    });
-    //console.log(Audio.find({}).count());
-    //var arr = uploads.find({}).toArray();
-
-    
-});
+// import './audiojs/audio.min.js';
+// var playlist = undefined;
+//
+// Template.Home.onRendered(function (){
+//     playlist = aeditorInit();
+//     aeditorEvent(playlist);
+//     console.log('lol');
+//
+//     audiojs.events.ready(function() {
+//         var as = audiojs.createAll();
+//     });
+//     //console.log(Audio.find({}).count());
+//     //var arr = uploads.find({}).toArray();
+//
+//
+// });
 Template.Home.helpers({
   uploads() {
   	return Audio.find({});

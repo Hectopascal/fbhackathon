f1e2d3aff653607e1e84b652c0ce200abfc7e65a
Hectopascal <zukisan@gmail.com>
2017-04-30 00:11:35 +1000

    decoder not working

--- fbhackathon/client/player.js
+++ fbhackathon/client/player.js
@@ -2,12 +2,61 @@ import { Template } from 'meteor/templating';
 import aeditorInit from './aeditorInit';
 import aeditorEvent from './aeditorEvent';
 import { Audio, uploads } from '../api/collections';
-
+import {AudioRecorder} from 'meteor/maxencecornet:audio-recorder';
+var recording = 0;
+var audioRecorder = new AudioRecorder();
 var playlist = undefined;
 
 Template.player.onRendered(function (){
     playlist = aeditorInit();
     aeditorEvent(playlist);
+});
+Template.player.events({
+
+    'click .start': function(){
+        
+        if(!recording){
+            audioRecorder.startRecording();
+            recording = 1;
+        }
+    },
+
+    'click .stop': function(){
+        if(recording) {
+            audioRecorder.stopRecording('Uint8Array', 'ArrayBufferFile', function (error, result) {
+                var data = new Blob([result],{type:'audio/mp3'});
+                //console.log('blah');
+                Audio.insert(new File([data],'input.mp3'),function (err, fileObj) {
+                    var userId = Meteor.userId();
+                    if (userId) {
+                      var obj = {
+                        userId: userId,
+                        audioId: fileObj._id
+                      };
+                      uploads.insert(obj);
+                      fileObj.on("uploaded", function () {
+                        var audioInfo = playlist.getInfo();
+                        console.log('/audio/' + 'audio-' + fileObj._id + '-' + fileObj.name());
+                        var path = '/audio/' + 'audio-' + fileObj._id + '-' + fileObj.name();
+                        audioInfo.push({
+                          src: path,
+                          name: fileObj.name(),
+                        });
+                        playlist.getEventEmitter().emit('clear');
+                        playlist.load(audioInfo);
+                      });
+                    }
+                });
+            });
+            //audioRecorder.stopRecording('wav', 'wavFile');
+             
+             //audioRecorder.stopRecording('wav', 'wavFile');
+
+            recording = 0;
+               
+        }
+        
+    }
 });
 
 Template.uploadForm.events({

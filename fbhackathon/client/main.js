f2fdca4b1b83f74464840444f7091793459a428e
Hectopascal <zukisan@gmail.com>
2017-04-29 19:16:35 +1000

    recorder update

--- fbhackathon/client/main.js
+++ fbhackathon/client/main.js
@@ -1,12 +1,10 @@
 import { Template } from 'meteor/templating';
-
+import './recording.js';
 import './main.html';
 import './loginRegister.html';
 import '../api/routes.js';
 import './loginRegister.js';
 import './profile.js';
-
-
 
 
 
